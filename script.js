7c6af053b9ef4e139c4113718fb77cd30b0060bc
DEV ARTSID <you@example.com>
2024-04-20 13:23:02 +0300

    Исправления

--- script.js
+++ script.js
@@ -116,81 +116,76 @@ const renderTasks = () => {
 	const categoryTasks = tasks.filter(
 		task => task.category.toLowerCase() === selectedCategory.title.toLowerCase()
 	)
-	// Проверка, есть ли задачи в выбранной категории
-	if (categoryTasks.length === 0) {
-		tasksContainer.innerHTML = `<p class="no-tasks">Задачи отсутствуют</p>`
-	} else {
-		// Рендеринг каждой задачи
-		categoryTasks.forEach(task => {
-			// Создание DOM-элемента для задачи и добавление обработчиков событий
-			const div = document.createElement('div')
-			div.classList.add('task-wrapper')
-			const label = document.createElement('label')
-			label.classList.add('task')
-			label.setAttribute('for', task.id)
-			const checkbox = document.createElement('input')
-			checkbox.type = 'checkbox'
-			checkbox.id = task.id
-			checkbox.checked = task.completed
-			checkbox.addEventListener('change', () => {
-				// Изменение состояния задачи при изменении чекбокса
-				const index = tasks.findIndex(t => t.id === task.id)
-				tasks[index].completed = !tasks[index].completed
-				saveLocal()
-			})
-			// Добавление содержимого в DOM-элемент задачи
-			div.innerHTML = `
-                <div class="delete">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5
-            `
-			label.innerHTML = `
-                <span class="checkmark">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M4.5 12.75l6 6 9-13.5"
-                        />
-                    </svg>
-                </span>
-                <p>${task.task}</p>
-            `
-			label.prepend(checkbox)
-			div.prepend(label)
-			tasksContainer.appendChild(div)
-			// Добавление обработчика события для удаления задачи
-			const deleteBtn = div.querySelector('.delete')
-			deleteBtn.addEventListener('click', () => {
-				const index = tasks.findIndex(t => t.id === task.id)
-				tasks.splice(index, 1)
-				saveLocal()
-				renderTasks()
-			})
+	// Рендеринг каждой задачи
+	categoryTasks.forEach(task => {
+		// Создание DOM-элемента для задачи и добавление обработчиков событий
+		const div = document.createElement('div')
+		div.classList.add('task-wrapper')
+		const label = document.createElement('label')
+		label.classList.add('task')
+		label.setAttribute('for', task.id)
+		const checkbox = document.createElement('input')
+		checkbox.type = 'checkbox'
+		checkbox.id = task.id
+		checkbox.checked = task.completed
+		checkbox.addEventListener('change', () => {
+			// Изменение состояния задачи при изменении чекбокса
+			const index = tasks.findIndex(t => t.id === task.id)
+			tasks[index].completed = !tasks[index].completed
+			saveLocal()
 		})
+		// Добавление содержимого в DOM-элемент задачи
+		div.innerHTML = `
+            <div class="delete">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5
+        `
+		label.innerHTML = `
+            <span class="checkmark">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M4.5 12.75l6 6 9-13.5"
+                    />
+                </svg>
+            </span>
+            <p>${task.task}</p>
+        `
+		label.prepend(checkbox)
+		div.prepend(label)
+		tasksContainer.appendChild(div)
+		// Добавление обработчика события для удаления задачи
+		const deleteBtn = div.querySelector('.delete')
+		deleteBtn.addEventListener('click', () => {
+			const index = tasks.findIndex(t => t.id === task.id)
+			tasks.splice(index, 1)
+			saveLocal()
+			renderTasks()
+		})
+	})
 
-		// Рендеринг категорий
-		renderCategories()
-		// Обновление счетчиков задач
-		updateTotals()
-	}
+	// Рендеринг категорий
+	renderCategories()
+	// Обновление счетчиков задач
+	updateTotals()
 }
 
 // Функция переключения формы добавления задачи
@@ -270,3 +265,6 @@ categories.forEach(category => {
 // Отображение начального состояния
 getLocal()
 renderTasks()
+
+// Отображение категорий независимо от наличия задач
+renderCategories()
